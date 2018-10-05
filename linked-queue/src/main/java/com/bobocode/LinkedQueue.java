5f5d7dc1f778f8efae007e18b61728f69df8c297
luckyloker <serhii.s.bondarenko@gmail.com>
2018-10-05 18:05:52 +0300

    Completed excercise

--- linked-queue/src/main/java/com/bobocode/LinkedQueue.java
+++ linked-queue/src/main/java/com/bobocode/LinkedQueue.java
@@ -6,23 +6,95 @@ package com.bobocode;
  * @param <T> a generic parameter
  */
 public class LinkedQueue<T> implements Queue<T> {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    /**
+     *
+     * Constructs LinkedQueue with default capacity;
+     *
+     */
+
+    LinkedQueue() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     *
+     * Constructs LinkedQueue with custom capacity;
+     *
+     */
+
+    LinkedQueue(int capacity) {
+        if (capacity < 0) {
+            elements = new Object[size];
+        }
+        else {
+            throw new IllegalArgumentException("Size can't be negative: " + size);
+        }
+    }
+
+
+    /**
+     *
+     * Adding element to LinkedQueue, if it's full create a new array with bigger capacity.
+     *
+     */
+
     @Override
     public void add(T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        if (size < elements.length) {
+        elements[size] = element;
+        }
+        else
+            {
+            Object[] temp = new Object[(size()*3)/2 + 1];
+            System.arraycopy(elements, 0, temp, 0, size());
+            elements = temp;
+            elements[size] = element;
+        }
+        size++;
+
     }
+
+    /**
+     *
+     * Poll element from LinkedQueue and shift queue with System.arraycopy;
+     * If queue is empty return null;
+     */
 
     @Override
     public T poll() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        T element = null;
+        if(size() != 0) {
+            element = (T) elements[0];
+            System.arraycopy(elements, 1, elements, 0, size()-1);
+            size--;
+
+        }
+        return element;
     }
+
+    /**
+     *
+     * Return size() of LinkedQueue;
+     *
+     */
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size;
     }
+
+    /**
+     *
+     * Check LinkedQueue, if it's empty return true;
+     *
+     */
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size == 0;
     }
 }
