0ca6a5233b55aa0615b105b45e02dbcb07f74360
luckyloker <serhii.s.bondarenko@gmail.com>
2018-10-11 14:59:34 +0300

    Completed exercise

--- linked-list/src/main/java/com/bobocode/LinkedList.java
+++ linked-list/src/main/java/com/bobocode/LinkedList.java
@@ -8,6 +8,19 @@ package com.bobocode;
  */
 public class LinkedList<T> implements List<T> {
 
+    private Node<T> head;
+    private int size;
+
+    static private class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        private Node(T element) {
+            this.element = element;
+        }
+
+    }
+
     /**
      * This method creates a list of provided elements
      *
@@ -16,7 +29,11 @@ public class LinkedList<T> implements List<T> {
      * @return a new list of elements the were passed as method parameters
      */
     public static <T> List<T> of(T... elements) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+       LinkedList<T> list = new LinkedList<>();
+       for (T element : elements) {
+           list.add(element);
+       }
+       return list;
     }
 
     /**
@@ -26,7 +43,18 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public void add(T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        Node<T> tempNode = new Node<>(element);
+        if (isEmpty()) {
+            head = tempNode;
+        }
+        else {
+            Node<T> tailNode = head;
+            while(tailNode.next!=null) {
+                tailNode = tailNode.next;
+            }
+            tailNode.next = tempNode;
+        }
+        size++;
     }
 
     /**
@@ -38,8 +66,23 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public void add(int index, T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
-    }
+        indexBoundCheck(index);
+        Node<T> tempNode = new Node<>(element);
+        if (index == 0) {
+            tempNode.next = head;
+            head = tempNode;
+            size++;
+        } else if (index == size) {
+            Node<T> indexNode = findIndex(index - 1);
+            indexNode.next = tempNode;
+            size++;
+        } else {
+            Node<T> indexNode = findIndex(index-1);
+            tempNode.next = indexNode.next;
+            indexNode.next = tempNode;
+            size++;
+            }
+        }
 
     /**
      * Changes the value of an list element at specific position. In case provided index in out of the list bounds it
@@ -50,7 +93,13 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public void set(int index, T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        indexBoundCheck(index);
+        if(index == size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> tempNode = new Node<>(element);
+        Node<T> indexNode = findIndex(index);
+        indexNode.element = tempNode.element;
     }
 
     /**
@@ -62,7 +111,14 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public T get(int index) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        indexBoundCheck(index);
+        if (size == 0 || index == size) {
+            throw new IndexOutOfBoundsException();
+        }
+        else {
+            Node<T> indexNode = findIndex(index);
+            return indexNode.element;
+        }
     }
 
     /**
@@ -74,9 +130,21 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public void remove(int index) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        indexBoundCheck(index);
+        int count = 0;
+        Node<T> indexNode = head;
+        if(index == 0) {
+            head = head.next;
+        }
+        else {
+        while(count != index - 1) {
+            indexNode = indexNode.next;
+            count++;
+        }
+        indexNode.next = indexNode.next.next;
+        size--;
     }
-
+    }
 
     /**
      * Checks if a specific exists in he list
@@ -85,7 +153,18 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public boolean contains(T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+       Node<T> indexNode = head;
+       if (size == 0) {
+           return false;
+       }
+       do {
+           if(indexNode.element.equals(element)) {
+               return true;
+           }
+           indexNode = indexNode.next;
+       }
+        while (indexNode.next != null);
+       return false;
     }
 
     /**
@@ -95,7 +174,7 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size == 0;
     }
 
     /**
@@ -105,7 +184,7 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public int size() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size;
     }
 
     /**
@@ -113,6 +192,23 @@ public class LinkedList<T> implements List<T> {
      */
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        head = null;
+        size = 0;
+    }
+
+    private void indexBoundCheck(int index) {
+        if(index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private Node<T> findIndex(int index) {
+        Node<T> indexNode = head;
+        int count = 0;
+        while (count != index) {
+            indexNode = indexNode.next;
+            count++;
+        }
+        return indexNode;
     }
 }
